8341ea3bfaef9ff0b743cdb085721b6260c8dcc0
Owen Helios <owenhelios@outlook.com>
2022-04-14 11:44:20 +0800

    sidebar toggle

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from 'components/Header'
+import Sidebar from 'components/Sidebar'
 import Home from 'pages/Home'
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom'
 import 'styles/Header.css'
 import DarkModeToggle from 'components/DarkModeToggle'
-
+import { FiMenu } from 'react-icons/fi'
 const Header = () => {
+  const openSidebar = () => {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar?.classList.add('open')
+  }
   return (
     <div className="header">
+      <button className="hamburger-btn" onClick={openSidebar}>
+        <i>
+          <FiMenu />
+        </i>
+      </button>
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
