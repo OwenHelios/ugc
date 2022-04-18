52570702f595192a983a2663c87a5a2e9dadeb22
Owen Helios <owenhelios@outlook.com>
2022-04-18 09:50:36 +0800

    'tinymce'

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,11 +1,51 @@
-import { Editor, EditorState } from 'draft-js'
-import { useState } from 'react'
-
+import React, { useRef } from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import { Editor as TinyMCEEditor } from 'tinymce'
 const Post = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
+  const editorRef = useRef<TinyMCEEditor | null>(null)
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent())
+    }
+  }
+  return (
+    <>
+      <Editor
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'preview',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
   )
-
-  return <Editor editorState={editorState} onChange={setEditorState} />
 }
 export default Post
